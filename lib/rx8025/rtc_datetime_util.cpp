92cc3a4c6217032e50a2a211ac1abbfc278d7f2e
CCY-STUDIO <yustart@foxmail.com>
2023-09-24 22:00:23 +0800

    update

---
+++ lib/rx8025/rtc_datetime_util.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: chenzedeng
+ * @Date: 2023-09-24 11:37:07
+ * @LastEditTime: 2023-09-24 20:44:09
+ */
+#include "rx8025.h"
+
+// 时间补偿获取
+
+rx8025_timeinfo last_timeinfo, current_timeinfo;
+
