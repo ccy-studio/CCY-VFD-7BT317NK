d90763e16ec39d142d87bab6dea3a739bd259dce
CCY-STUDIO <yustart@foxmail.com>
2023-10-09 23:54:04 +0800

    rgb上级测试初步成功

--- main/lib/Src/ws2812b.c
+++ main/lib/Src/ws2812b.c
@@ -5,6 +5,7 @@
  * @Date: 2023-10-09 17:18:28
  * @LastEditTime: 2023-10-09 17:58:31
  */
+#include <esp8266/gpio_struct.h>
 #include "ws2812b.h"
 
 typedef struct {
@@ -15,28 +16,30 @@ typedef struct {
 
 rgb_cache cache_arr[RGB_LED_COUNT];
 
-void delay_ns(u32 ns) {
-    while (ns--)
-        ;
-}
+static u8 buf_arr[RGB_LED_COUNT * 3];
+
 
 void reset() {
     gpio_set_level(RGB_PIN, 0);
-    delay_ms(350);
+    delay_ms(100);
 }
 
 void pin_low() {
-    gpio_set_level(RGB_PIN, 1);
-    delay_ns(300);
-    gpio_set_level(RGB_PIN, 0);
-    delay_ns(1090);
+    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
+    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
+    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
+    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
 }
 
 void pin_high() {
-    gpio_set_level(RGB_PIN, 1);
-    delay_ns(1090);
-    gpio_set_level(RGB_PIN, 0);
-    delay_ns(320);
+    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
+    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
 }
 
 void send_arr(u8 buf) {
@@ -51,11 +54,11 @@ void send_arr(u8 buf) {
     }
 }
 
-void send_grb(u8 g, u8 r, u8 b) {
-    send_arr(g);
-    send_arr(r);
-    send_arr(b);
-}
+//void send_grb(u8 g, u8 r, u8 b) {
+//    send_arr(g);
+//    send_arr(r);
+//    send_arr(b);
+//}
 
 void rbg_init() {
     PIN_INIT_NONE_OUTPUT(RGB_PIN);
@@ -72,9 +75,34 @@ void rgb_set_color(u8 index, u8 r, u8 g, u8 b) {
 }
 
 void rgb_update() {
+    memset(buf_arr, 0, sizeof(buf_arr));
+    u32 bi = 0;
     for (size_t i = 0; i < RGB_LED_COUNT; i++) {
         rgb_cache cache = cache_arr[i];
-        send_grb(cache.g, cache.r, cache.b);
+        buf_arr[bi++] = cache.g;
+        buf_arr[bi++] = cache.r;
+        buf_arr[bi++] = cache.b;
+    }
+    for (size_t i = 0; i < bi; i++) {
+        u8 data = buf_arr[i];
+        send_arr(data);
     }
     reset();
+
+    //高187ns 低 250ns
+    //t0h400ns t0l850ns
+//    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+//    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+//    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
+//    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
+//    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
+//    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
+//
+//    //t1h800ns t1l450ns
+//    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+//    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+//    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+//    GPIO.out_w1ts |= (0x1 << RGB_PIN); //1
+//    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
+//    GPIO.out_w1tc |= (0x1 << RGB_PIN);//0
 }
--- main/main.c
+++ main/main.c
@@ -14,13 +14,19 @@ void app_main() {
     uart_set_baudrate(UART_NUM_0, 115200);
     vfd_gui_init();
     vfd_gui_start();
+    rbg_init();
+
     delay_ms(1000);
     vfd_gui_set_text("Hello", 1);
     delay_ms(1000);
     vfd_gui_set_text("VFDT", 0);
+    u8 hue = 0;
     while (1) {
-        ESP_LOGI("main.c", "hello!\n");
-        delay_ms(500);
-        vfd_gui_set_text("VFDT", 0);
+        rgb_t res = rgb_heat_color(hue);
+        rgb_set_color(0, res.r, res.g, res.b);
+        rgb_set_color(1, res.r, res.g, res.b);
+        hue += 10;
+        rgb_update();
+        delay_ms(5);
     }
 }
