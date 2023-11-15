6faee8a40e02c04727ee4f7fad4774bc977d95a4
CCY-STUDIO <yustart@foxmail.com>
2023-11-15 21:20:23 +0800

    fix:修复清空WIFI信息失败的问题

--- main/lib/Src/smart_wifi.c
+++ main/lib/Src/smart_wifi.c
@@ -16,7 +16,8 @@ static volatile wifi_status_t current_wifi_state;
 static volatile u8 retry_count;  // 重试次数
 static volatile char ip[50];
 
-void wifi_set_ssid_pwd(char* ssid, char* pwd);
+void wifi_set_ssid_pwd(char *ssid, char *pwd);
+
 #if WIFI_CONF_TYPE == WIFI_CONF_ESPTOUCH
 #include "esp_smartconfig.h"
 #include "smartconfig_ack.h"
@@ -27,21 +28,26 @@ static const int ESPTOUCH_DONE_BIT = BIT1;
 static TaskHandle_t task_thread = NULL;
 static EventGroupHandle_t s_wifi_event_group = NULL;
 #else
+
 #include "cJSON.h"
+
 void wifi_start_softap();
+
 void wifi_stop_softap();
+
 void wifi_start_softap_server();
+
 static httpd_handle_t ap_server = NULL;
 extern const uint8_t ap_html_start[] asm("_binary_ap_html_start");
 extern const uint8_t ap_html_end[] asm("_binary_ap_html_end");
 static u16 sta_number = 0;
-static wifi_ap_record_t* ap_list_buffer = NULL;
+static wifi_ap_record_t *ap_list_buffer = NULL;
 #endif
 
-static void event_handler(void* arg,
+static void event_handler(void *arg,
                           esp_event_base_t event_base,
                           int32_t event_id,
-                          void* event_data) {
+                          void *event_data) {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
 #if WIFI_CONF_TYPE == WIFI_CONF_ESPTOUCH
         if (current_wifi_state == WIFI_CONFIGING) {
@@ -71,7 +77,7 @@ static void event_handler(void* arg,
         xEventGroupClearBits(s_wifi_event_group, CONNECTED_BIT);
 #endif
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
         memset(ip, 0, sizeof(ip));
         memcpy(ip, ip4addr_ntoa(&event->ip_info.ip), sizeof(ip));
         ESP_LOGI(APP_TAG, "Get ip:[%s]\n", ip);
@@ -84,49 +90,49 @@ static void event_handler(void* arg,
         http_start();
     }
 #if WIFI_CONF_TYPE == WIFI_CONF_ESPTOUCH
-    else if (event_base == SC_EVENT && event_id == SC_EVENT_FOUND_CHANNEL) {
-        ESP_LOGI(APP_TAG, "Found channel");
-    } else if (event_base == SC_EVENT && event_id == SC_EVENT_GOT_SSID_PSWD) {
-        ESP_LOGI(APP_TAG, "Got SSID and password");
-        smartconfig_event_got_ssid_pswd_t* evt =
-            (smartconfig_event_got_ssid_pswd_t*)event_data;
-        wifi_config_t wifi_config;
-        uint8_t ssid[33] = {0};
-        uint8_t password[65] = {0};
-        uint8_t rvd_data[33] = {0};
+        else if (event_base == SC_EVENT && event_id == SC_EVENT_FOUND_CHANNEL) {
+            ESP_LOGI(APP_TAG, "Found channel");
+        } else if (event_base == SC_EVENT && event_id == SC_EVENT_GOT_SSID_PSWD) {
+            ESP_LOGI(APP_TAG, "Got SSID and password");
+            smartconfig_event_got_ssid_pswd_t* evt =
+                (smartconfig_event_got_ssid_pswd_t*)event_data;
+            wifi_config_t wifi_config;
+            uint8_t ssid[33] = {0};
+            uint8_t password[65] = {0};
+            uint8_t rvd_data[33] = {0};
 
-        bzero(&wifi_config, sizeof(wifi_config_t));
-        memcpy(wifi_config.sta.ssid, evt->ssid, sizeof(wifi_config.sta.ssid));
-        memcpy(wifi_config.sta.password, evt->password,
-               sizeof(wifi_config.sta.password));
-        wifi_config.sta.bssid_set = evt->bssid_set;
+            bzero(&wifi_config, sizeof(wifi_config_t));
+            memcpy(wifi_config.sta.ssid, evt->ssid, sizeof(wifi_config.sta.ssid));
+            memcpy(wifi_config.sta.password, evt->password,
+                   sizeof(wifi_config.sta.password));
+            wifi_config.sta.bssid_set = evt->bssid_set;
 
-        if (wifi_config.sta.bssid_set == true) {
-            memcpy(wifi_config.sta.bssid, evt->bssid,
-                   sizeof(wifi_config.sta.bssid));
+            if (wifi_config.sta.bssid_set == true) {
+                memcpy(wifi_config.sta.bssid, evt->bssid,
+                       sizeof(wifi_config.sta.bssid));
+            }
+
+            memcpy(ssid, evt->ssid, sizeof(evt->ssid));
+            memcpy(password, evt->password, sizeof(evt->password));
+            ESP_LOGI(APP_TAG, "SSID:%s", ssid);
+            ESP_LOGI(APP_TAG, "PASSWORD:%s", password);
+            if (evt->type == SC_TYPE_ESPTOUCH_V2) {
+                ESP_ERROR_CHECK(
+                    esp_smartconfig_get_rvd_data(rvd_data, sizeof(rvd_data)));
+                ESP_LOGI(APP_TAG, "RVD_DATA:%s", rvd_data);
+            }
+            // 保存到NVS
+            wifi_set_ssid_pwd((char*)&ssid, (char*)&password);
+            ESP_ERROR_CHECK(esp_wifi_disconnect());
+            ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+            retry_count = 0;
+            ESP_ERROR_CHECK(esp_wifi_connect());
+            current_wifi_state = WIFI_CONNECTING;
         }
 
-        memcpy(ssid, evt->ssid, sizeof(evt->ssid));
-        memcpy(password, evt->password, sizeof(evt->password));
-        ESP_LOGI(APP_TAG, "SSID:%s", ssid);
-        ESP_LOGI(APP_TAG, "PASSWORD:%s", password);
-        if (evt->type == SC_TYPE_ESPTOUCH_V2) {
-            ESP_ERROR_CHECK(
-                esp_smartconfig_get_rvd_data(rvd_data, sizeof(rvd_data)));
-            ESP_LOGI(APP_TAG, "RVD_DATA:%s", rvd_data);
+        else if (event_base == SC_EVENT && event_id == SC_EVENT_SEND_ACK_DONE) {
+            xEventGroupSetBits(s_wifi_event_group, ESPTOUCH_DONE_BIT);
         }
-        // 保存到NVS
-        wifi_set_ssid_pwd((char*)&ssid, (char*)&password);
-        ESP_ERROR_CHECK(esp_wifi_disconnect());
-        ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-        retry_count = 0;
-        ESP_ERROR_CHECK(esp_wifi_connect());
-        current_wifi_state = WIFI_CONNECTING;
-    }
-
-    else if (event_base == SC_EVENT && event_id == SC_EVENT_SEND_ACK_DONE) {
-        xEventGroupSetBits(s_wifi_event_group, ESPTOUCH_DONE_BIT);
-    }
 #endif
 
 #if WIFI_CONF_TYPE == WIFI_CONF_AP
@@ -137,7 +143,7 @@ static void event_handler(void* arg,
 #endif
 }
 
-void wifi_get_ssid_pwd(char* ssid, char* pwd) {
+void wifi_get_ssid_pwd(char *ssid, char *pwd) {
     nvs_handle_t handle;
     if (nvs_open(NVS_WIFI_T, NVS_READONLY, &handle) != ESP_OK) {
         return;
@@ -157,7 +163,7 @@ void wifi_get_ssid_pwd(char* ssid, char* pwd) {
     ESP_LOGI(APP_TAG, "NVS Get ssid:%s pwd:%s", ssid, pwd);
 }
 
-void wifi_set_ssid_pwd(char* ssid, char* pwd) {
+void wifi_set_ssid_pwd(char *ssid, char *pwd) {
     nvs_handle_t handle;
     if (nvs_open(NVS_WIFI_T, NVS_READWRITE, &handle) != ESP_OK) {
         return;
@@ -173,16 +179,16 @@ void wifi_init() {
     if (s_wifi_event_group == NULL) {
         s_wifi_event_group = xEventGroupCreate();
 #endif
-        wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-        ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-        ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
-        ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
-                                                   &event_handler, NULL));
-        ESP_ERROR_CHECK(esp_event_handler_register(
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
+                                               &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(
             IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
 #if WIFI_CONF_TYPE == WIFI_CONF_ESPTOUCH
-        ESP_ERROR_CHECK(esp_event_handler_register(SC_EVENT, ESP_EVENT_ANY_ID,
-                                                   &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(SC_EVENT, ESP_EVENT_ANY_ID,
+                                               &event_handler, NULL));
 #endif
 
 #if WIFI_CONF_TYPE == WIFI_CONF_ESPTOUCH
@@ -246,12 +252,8 @@ wifi_status_t wifi_get_connect_state(void) {
 }
 
 void wifi_erase_config(void) {
-    nvs_handle_t handle;
-    if (nvs_open(NVS_WIFI_T, NVS_READWRITE, &handle) != ESP_OK) {
-        return;
-    }
-    nvs_erase_all(handle);
-    nvs_close(handle);
+    ESP_ERROR_CHECK(nvs_flash_erase())
+    ESP_ERROR_CHECK(nvs_flash_init())
 }
 
 #if WIFI_CONF_TYPE == WIFI_CONF_ESPTOUCH
@@ -301,11 +303,11 @@ void wifi_start_softap() {
     }
     current_wifi_state = WIFI_CONFIGING;
     wifi_config_t wifi_config = {
-        .ap = {.ssid = WIFI_SOFTAP_SSID,
-               .ssid_len = strlen(WIFI_SOFTAP_SSID),
-               .password = "",
-               .max_connection = 1,
-               .authmode = WIFI_AUTH_OPEN},
+            .ap = {.ssid = WIFI_SOFTAP_SSID,
+                    .ssid_len = strlen(WIFI_SOFTAP_SSID),
+                    .password = "",
+                    .max_connection = 1,
+                    .authmode = WIFI_AUTH_OPEN},
     };
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
@@ -317,7 +319,7 @@ void wifi_stop_softap() {
     ap_server = NULL;
 }
 
-void wifi_ap_close_start_sta(void* params) {
+void wifi_ap_close_start_sta(void *params) {
     delay_ms(1000);
     wifi_stop_softap();
     char ssid[32], pwd[64];
@@ -336,9 +338,9 @@ void wifi_ap_close_start_sta(void* params) {
     vTaskDelete(NULL);
 }
 
-static esp_err_t index_handle(httpd_req_t* req) {
+static esp_err_t index_handle(httpd_req_t *req) {
     httpd_resp_set_hdr(req, CONTENT_TYPE_HTML);
-    char* str = (char*)malloc(ap_html_end - ap_html_start);
+    char *str = (char *) malloc(ap_html_end - ap_html_start);
     if (str == NULL) {
         ESP_LOGE(APP_TAG, "Mem Init Fail Err");
         httpd_resp_send_500(req);
@@ -349,7 +351,8 @@ static esp_err_t index_handle(httpd_req_t* req) {
     free(str);
     return ESP_OK;
 }
-static esp_err_t scan_ap_handle(httpd_req_t* req) {
+
+static esp_err_t scan_ap_handle(httpd_req_t *req) {
     esp_wifi_scan_stop();
     wifi_scan_config_t scan_config = {0};
     scan_config.scan_time.active.max = 500;  // ms
@@ -366,23 +369,23 @@ static esp_err_t scan_ap_handle(httpd_req_t* req) {
         return ESP_FAIL;
     }
     esp_wifi_scan_get_ap_records(&sta_number,
-                                 (wifi_ap_record_t*)ap_list_buffer);
+                                 (wifi_ap_record_t *) ap_list_buffer);
 
     httpd_resp_set_hdr(req, CONTENT_TYPE_JSON);
     // 构建JSON数据
-    char* jsonStr = NULL;
-    cJSON* root = cJSON_CreateArray();
+    char *jsonStr = NULL;
+    cJSON *root = cJSON_CreateArray();
     if (root == NULL) {
         goto end;
     }
     for (size_t i = 0; i < sta_number; i++) {
         // ESP_LOGI(APP_TAG, "[%s][rssi=%d]", ap_list_buffer[i].ssid,
         //          ap_list_buffer[i].rssi);
-        cJSON* obj = cJSON_CreateObject();
+        cJSON *obj = cJSON_CreateObject();
         if (obj == NULL) {
             goto end;
         }
-        cJSON_AddStringToObject(obj, "ssid", (char*)ap_list_buffer[i].ssid);
+        cJSON_AddStringToObject(obj, "ssid", (char *) ap_list_buffer[i].ssid);
         cJSON_AddNumberToObject(obj, "rssi", ap_list_buffer[i].rssi);
         cJSON_AddItemToArray(root, obj);
     }
@@ -394,7 +397,7 @@ static esp_err_t scan_ap_handle(httpd_req_t* req) {
     httpd_resp_send(req, jsonStr, strlen(jsonStr));
     free(jsonStr);
 
-end:
+    end:
     cJSON_Delete(root);
     if (ap_list_buffer != NULL) {
         free(ap_list_buffer);
@@ -403,10 +406,10 @@ end:
     return ESP_OK;
 }
 
-static esp_err_t wifi_connect_handle(httpd_req_t* req) {
+static esp_err_t wifi_connect_handle(httpd_req_t *req) {
     httpd_resp_set_hdr(req, CONTENT_TYPE_HTML);
     // 获取请求内的表单数据
-    char* buf;
+    char *buf;
     size_t buf_len;
     char ssid[25] = {0}, pwd[25] = {0};
 
@@ -434,23 +437,25 @@ static esp_err_t wifi_connect_handle(httpd_req_t* req) {
         wifi_set_ssid_pwd(&ssid, &pwd);
         xTaskCreate(wifi_ap_close_start_sta, "WIFI-RSTA", 4096, NULL, 5, NULL);
     }
-    char result[25] = "正在尝试连接Wi-Fi";
+    char result[84] = "发送Wi-Fi链接信息到设备成功,请转而查看VFD屏幕的WiFi状态变化";
     httpd_resp_send(req, result, strlen(result));
     return ESP_OK;
 }
+
 static httpd_uri_t index_t = {.uri = "/",
-                              .method = HTTP_GET,
-                              .handler = index_handle,
-                              .user_ctx = NULL};
+        .method = HTTP_GET,
+        .handler = index_handle,
+        .user_ctx = NULL};
 static httpd_uri_t scan_ap_t = {.uri = "/scan-ap",
-                                .method = HTTP_GET,
-                                .handler = scan_ap_handle,
-                                .user_ctx = NULL};
+        .method = HTTP_GET,
+        .handler = scan_ap_handle,
+        .user_ctx = NULL};
 
 static httpd_uri_t wifi_connect_t = {.uri = "/wifi-connect",
-                                     .method = HTTP_GET,
-                                     .handler = wifi_connect_handle,
-                                     .user_ctx = NULL};
+        .method = HTTP_GET,
+        .handler = wifi_connect_handle,
+        .user_ctx = NULL};
+
 void wifi_start_softap_server() {
     if (ap_server != NULL) {
         return;
@@ -467,8 +472,9 @@ void wifi_start_softap_server() {
         ESP_LOGI(APP_TAG, "Error starting  AP server!");
     }
 }
+
 #endif
 
-char* wifi_get_ip() {
-    return (char*)&ip[0];
+char *wifi_get_ip() {
+    return (char *) &ip[0];
 }
